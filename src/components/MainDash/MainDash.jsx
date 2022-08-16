a1608fd6ad5a8e323afd26580b88f333acec73df
sebastiap <sebastiap@gmail.com>
2022-08-15 22:52:38 -0300

    main dash created

---
+++ src/components/MainDash/MainDash.jsx
@@ -0,0 +1,15 @@
+import React,{useState} from "react";
+
+const MainDash = () => {
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <Sidebar/>
+        <div/>
+        <div/>
+      </div>
+    </div>
+  );
+}
+
+export default MainDash;
