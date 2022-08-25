4969cce97bf10c2471936bf192ea7a7da9695450
sebastiap <sebastiap@gmail.com>
2022-08-25 14:49:12 -0300

    Starting Cards

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -40,3 +40,16 @@ import {
       heading: 'Analytics'
     },
   ];
+
+  export const CardsData = [
+    {title: "Sales", 
+    color: {backgroundColor: "linear-gradient(180deg,#bb67ff 0%, #c484f3 100%)",
+    boxShadow:"0px 10px 20px 0px #e0c6f5"},
+    bacValue:70,
+    value: "25,970",
+    //png: UilUsdSquare,
+    series: [{
+      name:"Sales",
+      data: [31,40,28,51,42,109,100]
+    }]},
+    {},{}]
