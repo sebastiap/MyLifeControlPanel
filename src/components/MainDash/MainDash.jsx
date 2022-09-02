b6c24604b74b060ea641444030fd68e1f467075a
sebastiap <sebastiap@gmail.com>
2022-09-02 16:59:01 -0300

    Finished Structure. Only own data needed

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -1,13 +1,16 @@
 import React,{useState} from "react";
 import './MainDash.css'
 import Cards from "../Cards/Cards"
+import Table from "../Table/Table";
 
 const MainDash = () => {
   return (
     <div className="MainDash">
     <h1>Dashboard</h1>
       <Cards/>
+      <Table/>
     </div>
+
   );
 }
 
